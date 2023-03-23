43337d763f56aa2763991c6514220bea75a19276
atashie <51334454+atashie@users.noreply.github.com>
2023-03-23 11:17:17 -0400

    weekly updates
    weekly updates

--- surfaceWaterModeling_seasonal_interface_forWeeklyUpdates.r
+++ surfaceWaterModeling_seasonal_interface_forWeeklyUpdates.r
@@ -1,6 +1,8 @@
 	# reading in the ncdfs to ensure timing / structure 
 library(ncdf4)			# for loading netcdf that contains lat-lons of climate data
 library(data.table)		# for data.table
+
+source("C:\\Users\\arik\\Documents\\GitHub\\SurfaceWaterProjections\\surfaceWaterModeling_functions.r")
 	
 	# example input data
 theseBasins = data.frame(basinSymbol = 	c('CLE',		'ISB',		'EXC',		'ORO',		'SHA',		'FOL',		'PNF',		'MIL',		'TRM'),
@@ -9,8 +11,8 @@ theseBasins = data.frame(basinSymbol = 	c('CLE',		'ISB',		'EXC',		'ORO',		'SHA',
 						incldStorage = 	c(TRUE, 		TRUE,		TRUE,		TRUE,		TRUE,		TRUE,		TRUE,		FALSE,		FALSE),
 						maxStorage = 	c(2447650,		568000,		1024600,	3537577,	4552000,	977000,		1000000,	520500,		185600))
 
-yesterdaysDate = '2023-01-16'	# historic data is released every day for the day prior
-forecastDate = '17JAN2023'		# 
+yesterdaysDate = '2023-02-26'	# historic data is released every day for the day prior
+forecastDate = '27FEB2023'		# 
 waterYearStart = as.Date('2022-10-01')
 
 	# these file locations remain the same for all watersheds of a region
@@ -24,7 +26,7 @@ basinATLAS_locationAndFile = 'C:\\Users\\arik\\Documents\\PhD Research\\D4\\Basi
 #cfsStartDate = as.Date('2022-02-28') #  + ncvar_get(ncin_cfs, 'time')/24 for calculating actual dates
 era5StartDate =  as.Date('2020-08-01') # + ncvar_get(ncin_era5, 'time') for calculating actual dates 
 #era5RecentStartDate =  as.Date('2001-07-01') # + ncvar_get(ncin_recentEra5, 'time') for calculating actual dates 
-seas5StartDate = as.Date('2023-01-01') # + ncvar_get(ncin_seas5, 'lead_time') for calculating actual dates
+seas5StartDate = as.Date('2023-02-01') # + ncvar_get(ncin_seas5, 'lead_time') for calculating actual dates
 
 		### this section is purely for inspecting data before running models
 #########################################################################
